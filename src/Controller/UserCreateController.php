a7c9646a8519515062293b56483946c879f60eb1
218566 <218566@student.pwr.edu.pl>
2020-03-02 18:39:11 +0100

    UserCreate

--- src/Controller/UserCreateController.php
+++ src/Controller/UserCreateController.php
@@ -3,17 +3,25 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class UserLoginController extends AbstractController
+class UserCreateController extends AbstractController
 {
     /**
-     * @Route("/login", name="user_login")
+     * @Route("/create/user, name="user_create")
      */
-    public function loginAction()
+    public function createAction()
     {
+
+        $user = new User()
+
+
+
+
+
         return new Response(
             '<html><body>Chuj</body></html>'
         );
